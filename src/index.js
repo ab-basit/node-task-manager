c658c17ab92a6450120897472be993fe5705fdd1
Abdul Basit <basitthag@gmail.com>
2019-09-01 17:46:55 +0530

    Added CORS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const taskRouter = require('./routers/task');
 const userRouter = require('./routers/user');
+const cors = require('cors');
 require('./db/mongoose');
 
 
 const app = express();
 const port = process.env.PORT;
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-app.listen(port, () => console.log(`listening at port ${port}`))
-
-
+app.listen(port, () => console.log(`listening at port ${port}`));
